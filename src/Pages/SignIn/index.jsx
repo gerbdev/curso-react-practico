869e0049543f2e7ffdb7b00cca1c3ed355347045
gerbdev <gerdabaralv@gmail.com>
2023-11-15 20:26:51 -0500

    Add/local state for the view/of account or log in

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,10 +1,11 @@
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import Layout from "../../Components/Layout";
-import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
+import Layout from "../../Components/Layout";
 
 function SignIn() {
   const context = useContext(ShoppingCartContext);
+  const [view, setView] = useState("user-info");
 
   // Account
   const account = localStorage.getItem("account");
@@ -18,9 +19,8 @@ function SignIn() {
     : true;
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
 
-  return (
-    <Layout>
-      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+  const renderLoginIn = () => {
+    return (
       <div className="flex flex-col w-80">
         <p>
           <span className="font-light text-sm">Email: </span>
@@ -50,10 +50,25 @@ function SignIn() {
           className="border border-black disabled:text-black/40 disabled:border-black/40
           rounded-lg mt-6 py-3"
           disabled={hasUserAnAccount}
+          onClick={() => setView("create-user-info")}
         >
           Sign up
         </button>
       </div>
+    );
+  };
+
+  const renderCreateUserInfo = () => {
+    // TODO: create render view
+  };
+
+  const renderView = () =>
+    view === "create-user-info" ? renderCreateUserInfo() : renderLoginIn();
+
+  return (
+    <Layout>
+      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+      {renderView()}
     </Layout>
   );
 }
