c2cf151ba15be8fa91bb5b0ae254f6ac8e0e72b9
gerbdev <gerdabaralv@gmail.com>
2023-11-15 21:31:38 -0500

    Add/ onClick at SignIn/ in the button Log in

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -59,6 +59,7 @@ function SignIn() {
         <Link to="/">
           <button
             className="bg-black disabled:bg-black/40 text-white  w-full rounded-lg py-3 mt-4 mb-2"
+            onClick={() => handleSignIn()}
             disabled={!hasUserAnAccount}
           >
             Log in
