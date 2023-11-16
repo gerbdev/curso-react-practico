37c02e9c0bfc086a9819f6fe4be39764b7a49260
gerbdev <gerdabaralv@gmail.com>
2023-11-15 20:00:17 -0500

    add/validation/ user account or no

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
 import Layout from "../../Components/Layout";
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 
 function SignIn() {
+  const context = useContext(ShoppingCartContext);
+
+  // Account
+  const account = localStorage.getItem("account");
+  const parsedAccount = JSON.parse(account);
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount
+    ? Object.keys(parsedAccount).length === 0
+    : true;
+  const noAccountInLocalState = context.account
+    ? Object.keys(context.account).length === 0
+    : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
   return (
     <Layout>
       <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
       <div className="flex flex-col w-80">
         <p>
           <span className="font-light text-sm">Email: </span>
-          <span>gerdev@platzi.com</span>
+          <span>{parsedAccount?.email}</span>
         </p>
         <p>
           <span className="font-light text-sm">Password: </span>
-          <span>******</span>
+          <span>{parsedAccount?.password}</span>
         </p>
         <Link to="/">
-          <button className="bg-black disabled:bg-black/40 text-white  w-full rounded-lg py-3 mt-4 mb-2">
+          <button
+            className="bg-black disabled:bg-black/40 text-white  w-full rounded-lg py-3 mt-4 mb-2"
+            disabled={!hasUserAnAccount}
+          >
             Log in
           </button>
         </Link>
@@ -30,6 +49,7 @@ function SignIn() {
         <button
           className="border border-black disabled:text-black/40 disabled:border-black/40
           rounded-lg mt-6 py-3"
+          disabled={hasUserAnAccount}
         >
           Sign up
         </button>
